d533faec782f1b73bcc024640f97e97bf2517790
MaksimCharyev <helloimaksim@gmail.com>
2023-06-09 09:58:06 +0300

    Done Library with tests

---
+++ SquareLibrary/SquareLibrary.cs
@@ -0,0 +1,58 @@
+﻿namespace SquareLibrary
+{
+    interface ISquare
+    {
+        double CalculateSquare();
+    }
+    public class Triangle
+    {
+        private List<double> _sides; //Используется для проверок
+        private double _sideOne;
+        private double _sideTwo;
+        private double _sideThree;
+        public double SideOne { get { return this._sideOne; } set { if (value > 0) { this._sideOne = value; } else { throw new ArgumentException("Значение не может быть отрицательным.", nameof(value)); } } }
+        public double SideTwo { get { return this._sideTwo; } set { if (value > 0) { this._sideTwo = value; } else { throw new ArgumentException("Значение не может быть отрицательным.", nameof(value)); } } }
+        public double SideThree { get { return this._sideThree; } set { if (value > 0) { this._sideThree = value; } else { throw new ArgumentException("Значение не может быть отрицательным.", nameof(value)); } } }
+        public Triangle(double FirstSide, double SecondSide, double ThirdSide)
+        {
+            var Sum = FirstSide + SecondSide + ThirdSide;
+            _sides = new List<double>() { FirstSide, SecondSide, ThirdSide };
+            foreach (var s in _sides)
+            {
+                if (s >= (Sum - s))
+                {
+                    throw new ArgumentOutOfRangeException("Треугольник с такими сторонами не существует");
+                }
+            }
+            SideOne = FirstSide;
+            SideTwo = SecondSide;
+            SideThree = ThirdSide;
+        }
+        public double CalculateSquare()
+        {
+            var HalfPerimeter = (_sideOne + _sideTwo + _sideThree) / 2f;
+            return Math.Sqrt(HalfPerimeter * (HalfPerimeter - _sideOne) * (HalfPerimeter - _sideTwo) * (HalfPerimeter - _sideThree));
+        }
+
+        public bool IsRectangular()
+        {
+            _sides.Sort();
+
+            return Math.Pow(_sides[0], 2) + Math.Pow(_sides[1], 2) == Math.Pow(_sides[2], 2);
+        }
+    }
+
+    public class Circle : ISquare
+    {
+        private double _radius;
+        public double Radius { get { return this._radius; } set { if (value > 0) { this._radius = value; } else { throw new ArgumentException("Значение не может быть отрицательным.", nameof(value)); } } }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double CalculateSquare()
+        {
+            return Math.PI*Math.Pow(_radius, 2);
+        }
+    }
+}
---
+++ SquareLibraryTests/SquareLibraryTests.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SquareLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareLibrary.Tests
+{
+    [TestClass()]
+    public class SquareLibraryTests
+    {
+        [TestMethod()]
+        public void TriangleTest_Constructor_with_valid_data()
+        {
+            // Arrange
+            double side1 = 2;
+            double side2 = 3;
+            double side3 = 4;
+
+            //Act
+            Triangle triangle = new Triangle(side1, side2, side3);
+
+            // Assert
+            Assert.IsNotNull(triangle);
+            Assert.AreEqual(side1, triangle.SideOne);
+            Assert.AreEqual(side2, triangle.SideTwo);
+            Assert.AreEqual(side3, triangle.SideThree);
+        }
+        [TestMethod()]
+        public void TriangleTest_Constructor_with_invalid_data()
+        {
+            // Arrange
+            double side1 = -2;
+            double side2 = -3;
+            double side3 = -4;
+
+            //Act
+            Triangle triangle = null;
+
+            // Assert
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => { triangle = new Triangle(side1, side2, side3); });
+            
+        }
+        [TestMethod()]
+        public void TriangleTest_Square()
+        {
+            //Arrange
+            double side1 = 5;
+            double side2 = 3;
+            double side3 = 4;
+            double expected = 6;
+
+            //Act
+            Triangle triangle = new Triangle(side1, side2, side3);
+            var square = triangle.CalculateSquare();
+
+            //Assert
+            Assert.AreEqual(expected, square);
+        }
+        [TestMethod()]
+        public void TriangleTest_Rectangular()
+        {
+            //Arrange
+            double side1 = 5;
+            double side2 = 3;
+            double side3 = 4;
+            
+            //Act
+            Triangle triangle = new Triangle(side1, side2, side3);
+
+            //Assert
+            Assert.IsTrue(triangle.IsRectangular());
+        }
+        [TestMethod()]
+        public void CircleTest_Constructor_with_valid_data()
+        {
+            //Arrange
+            double radius = 5;
+
+            //Act
+            Circle circle = new Circle(radius);
+
+            //Assert
+            Assert.AreEqual(radius, circle.Radius);
+        }
+        [TestMethod()]
+        public void CircleTest_Constructor_with_invalid_data()
+        {
+            //Arrange
+            double radius = -5;
+            Circle circle = null;
+
+            //Act
+            
+
+            //Assert
+            Assert.ThrowsException<ArgumentException>(() => { circle = new Circle(radius); });
+        }
+        [TestMethod()]
+        public void CircleTest_Square()
+        {
+            //Arrange
+            double radius = 17.5;
+            double expected = Math.PI*radius*radius;
+
+            //Act
+            Circle circle = new Circle(radius);
+            var square = circle.CalculateSquare();
+
+            //Assert
+            Assert.AreEqual(expected, square);
+        }
+    }
+}
